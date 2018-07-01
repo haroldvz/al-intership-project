b1868260bc483d3567a1e37998468c27296a0117
haroldvz <velezh010@gmail.com>
2018-07-01 12:23:28 -0500

    FlexLayout replaced by Material Grid List in movie-cast component and movie-crew component, gaps between cards fixed, responsive breakpoints added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-//import { HttpModule, JsonpModule } from '@angular/http';
-import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
-
-import {
-  MatButtonModule, MatListModule, MatIconModule, MatMenuModule, MatInputModule, 
-  MatSelectModule,  MatToolbarModule,
-  MatTabsModule,
-  MatFormFieldModule
-} from '@angular/material';
-
-
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
-
-import {
-  CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule,
-  CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
-  CovalentNotificationsModule, CovalentMenuModule, CovalentDataTableModule, CovalentMessageModule, TdMediaService
-} from '@covalent/core';
+import { TdMediaService } from '@covalent/core';
 import { CommonModule } from '@angular/common';
 
 import { MoviesModule } from './movies/movies.module';
@@ -33,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { TvSeriesModule } from './tv-series/tv-series.module';
 import { NotFoundComponent } from './shared/layout/not-found/not-found.component';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -44,58 +26,13 @@ import { NotFoundComponent } from './shared/layout/not-found/not-found.component
     FormsModule,
     RouterModule.forRoot([]),
     HttpClientModule,
-    /** Material Modules */
-    MatButtonModule,
-    MatListModule,
-    MatIconModule,
-    //MatCardModule,
-    MatMenuModule,
-    //MatInputModule,
-    //MatSelectModule,
-    //MatButtonToggleModule,
-    //MatSlideToggleModule,
-    //MatProgressSpinnerModule,
-    //MatDialogModule,
-    //MatSnackBarModule,
-    MatToolbarModule,
-    MatTabsModule,
-    //MatSidenavModule,
-    //MatTooltipModule,
-    //MatRippleModule,
-    /*MatRadioModule,
-    MatGridListModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSliderModule,
-    MatAutocompleteModule,*/
-    /**FontAwesomeModule */
-    AngularFontAwesomeModule,
-    /** Covalent Modules */
-    CovalentCommonModule,
-    CovalentLayoutModule,
-    CovalentMediaModule,
-    CovalentExpansionPanelModule,
-    CovalentStepsModule,
-    CovalentDialogsModule,
-    CovalentLoadingModule,
-    CovalentSearchModule,
-    CovalentPagingModule,
-    CovalentNotificationsModule,
-    CovalentMenuModule,
-    CovalentDataTableModule,
-    CovalentMessageModule,
-
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
     //My modules
     MoviesModule,
     PeopleModule,
     TvSeriesModule,
     AppRoutingModule,
-
-    /*Gallery*/
-    
+    //shared-module
+    SharedModule,
   ],
   declarations: [
     AppComponent,
---
+++ src/app/shared/shared-material.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule, MatSelectModule, MatInputModule, MatTabsModule, MatIconModule, MatToolbarModule, MatGridListModule, MatCardModule, MatButtonModule, MatListModule, MatMenuModule } from '@angular/material';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTabsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatTabsModule,
+    AngularFontAwesomeModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTabsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatTabsModule,
+    AngularFontAwesomeModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
+  declarations: []
+})
+export class SharedMaterialModule { }
