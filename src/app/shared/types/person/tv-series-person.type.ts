00b5e57047ee3be54fb4de024214bdbbea7b6622
haroldvz <velezh010@gmail.com>
2018-07-02 00:44:39 -0500

    tv-series-person type added to shared directory

---
+++ src/app/shared/types/person/tv-series-person.type.ts
@@ -0,0 +1,79 @@
+import { GenreDescriptor } from "../genres/genres.type";
+
+export class CreditsTVPersonDescriptor {
+
+    public id: number;
+    public cast: CastCreditsTVPeopleDescriptor[] = [];
+    //public crew: CrewDescriptor[] = [];
+
+    /**
+     *
+     *
+     * @static
+     * @param {*} rawData
+     * @returns
+     * @memberof CreditsDescriptor
+     */
+    public static import(rawData: any): CreditsTVPersonDescriptor {
+        let credits: CreditsTVPersonDescriptor = new CreditsTVPersonDescriptor();
+
+
+        credits.id = rawData.hasOwnProperty('id') ? rawData.id : null;
+
+
+        let cast: CastCreditsTVPeopleDescriptor;
+        if (rawData.hasOwnProperty("cast")) {
+            console.log(rawData.cast.length);
+            for (let i = 0; i < rawData.cast.length; i++) {
+                let row: any = rawData.cast[i];
+                cast = CastCreditsTVPeopleDescriptor.import(row);
+                credits.cast.push(cast);
+            }
+        }
+
+
+        return credits;
+    }
+}
+
+
+
+
+export class CastCreditsTVPeopleDescriptor {
+    public character: string;
+    public credit_id: string;
+    public id: number;
+    public title: string;
+    public poster_path: string;
+    public video: boolean;
+    public genres: GenreDescriptor[] = [];
+    public release_date: string;
+    public overview: string;
+    public popularity: number;
+    public original_language: string;
+    public vote_count:number;
+    public backdrop_path:string;
+    public vote_average:number;
+
+
+    public static import(rawData: any) {
+        let castCreditsTVPeopleDescriptor: CastCreditsTVPeopleDescriptor = new CastCreditsTVPeopleDescriptor();
+
+        castCreditsTVPeopleDescriptor.character = rawData.hasOwnProperty('character') ? rawData.character : '';
+        castCreditsTVPeopleDescriptor.credit_id = rawData.hasOwnProperty('credit_id') ? rawData.credit_id : '';
+        castCreditsTVPeopleDescriptor.id = rawData.hasOwnProperty('id') ? rawData.id : -1;
+        castCreditsTVPeopleDescriptor.title = rawData.hasOwnProperty('title') ? rawData.title : '';
+        castCreditsTVPeopleDescriptor.poster_path = rawData.hasOwnProperty('poster_path') ? rawData.poster_path : '';
+        castCreditsTVPeopleDescriptor.backdrop_path = rawData.hasOwnProperty('backdrop_path') ? rawData.backdrop_path : '';
+        
+        
+        castCreditsTVPeopleDescriptor.release_date = rawData.hasOwnProperty('release_date') ? rawData.release_date : '';
+        castCreditsTVPeopleDescriptor.overview = rawData.hasOwnProperty('overview') ? rawData.overview : '';
+        castCreditsTVPeopleDescriptor.popularity = rawData.hasOwnProperty('popularity') ? rawData.popularity : -1;
+        castCreditsTVPeopleDescriptor.original_language = rawData.hasOwnProperty('original_language') ? rawData.original_language : '';
+        castCreditsTVPeopleDescriptor.vote_count = rawData.hasOwnProperty('vote_count') ? rawData.vote_count : -1;
+        castCreditsTVPeopleDescriptor.vote_average = rawData.hasOwnProperty('vote_average') ? rawData.vote_average : -1;
+        
+        return castCreditsTVPeopleDescriptor;
+    }
+}
