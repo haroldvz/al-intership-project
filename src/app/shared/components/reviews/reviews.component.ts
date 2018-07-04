c9a448364b40303a288a47d21a5e88205f8af4c8
haroldvz <velezh010@gmail.com>
2018-07-04 18:07:58 -0500

    reviews component added to movie-detail, card styles improved in review-card component

--- src/app/shared/components/reviews/reviews.component.ts
+++ src/app/shared/components/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
+  @Input() public data;
+
+
   constructor() { }
 
   ngOnInit() {
+
+
+
+
   }
 
 }
