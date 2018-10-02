515e5642f8da48e1eb11f270a0bc341bc1a47da3
Harold Velez <haroldvelez@AL-COL-02693.local>
2018-10-02 16:55:50 -0500

    work in global handler service

--- src/app/shared/components/error/error.component.ts
+++ src/app/shared/components/error/error.component.ts
@@ -1,40 +1,36 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { GlobalErrorHandlerService } from '../../services/global-error-handler.service';
 import { map, take } from 'rxjs/operators';
 import { Router, NavigationEnd } from '@angular/router';
-
+import { NgZone } from '@angular/core';
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  providers: [GlobalErrorHandlerService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./error.component.scss']
 })
 export class ErrorComponent implements OnInit, OnDestroy {
 
   error_message: any;
-  mc = '';
-  navigationSubscription;
 
-  constructor(private global_error_handler_service: GlobalErrorHandlerService, private router: Router) {
-    
-    this.mc = this.global_error_handler_service.m;
-    this.global_error_handler_service.getSubject().subscribe((data)=>{
-      console.log('new subscribe');
-      this.error_message = data;
-    });
-    
+  constructor(public zone: NgZone,private global_error_handler_service: GlobalErrorHandlerService, private router: Router) {
+    this.zone.run(() => { this.router.navigate(['/error']); });  
+    this.error_message = this.global_error_handler_service.getSubject();
+    /*this.global_error_handler_service.getSubject().subscribe((data)=>{
+        console.log('new subscribe');
+        this.error_message = data;
+
+        console.log(this.error_message);
+      });*/
     
   }
 
-  
   ngOnInit() {
 
   }
 
   ngOnDestroy() {
-    if (this.navigationSubscription) {
-      this.navigationSubscription.unsubscribe();
-    }
+   
   }
 
 
